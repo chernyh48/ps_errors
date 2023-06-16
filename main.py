8d8bba6c4b5ae7088d39dff71c81678dac882b4c
Alexandr <a.chernyh.1991@mail.ru>
2023-06-16 11:38:47 +0300

    config

--- main.py
+++ main.py
@@ -2,25 +2,29 @@ import telebot
 import codecs
 import shlex
 import subprocess
+from config import *
 
-
-API_TOKEN = '6218362175:AAEdq9dM10yePoTtK0mrns6HD2kbTP7x4P4'
+API_TOKEN = API_TOKEN
 bot = telebot.TeleBot(API_TOKEN)
-chat_id = '-1001942641270'
+chat_id = chat_id
 
-w = "%{http_code}"
-with open('proxy.txt', 'r') as f:
-    proxys = f.readlines()
-
-for i in proxys:
-    proxy = i.rstrip('\n').split(':')
-    curl_url = f'curl -x "http://{proxy[2]}:{proxy[3]}@{proxy[0]}:{proxy[1]}" -w {w} https://2ip.ru'
-    args = shlex.split(curl_url)
-    process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    try:
-        stdout, stder = process.communicate(timeout=2)
-        data = codecs.decode(stdout)
-        if data == '000':
-            bot.send_message(chat_id, f'\U0000274C {i}')
-    except subprocess.TimeoutExpired:
-        bot.send_message(chat_id, f'\U0000274C {i}')
+try:
+    result = ''
+    w = "%{http_code}"
+    with open('proxy.txt', 'r') as f:
+        proxys = f.readlines()
+    for i in proxys:
+        proxy = i.rstrip('\n').split(':')
+        curl_url = f'curl -x "http://{proxy[2]}:{proxy[3]}@{proxy[0]}:{proxy[1]}" -w {w} https://2ip.ru'
+        args = shlex.split(curl_url)
+        process = subprocess.Popen(args, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        try:
+            stdout, stder = process.communicate(timeout=2)
+            data = codecs.decode(stdout)
+            if data == '000':
+                result += f'\U0000274C {i}\n'
+        except subprocess.TimeoutExpired:
+            result += f'\U0000274C {i}\n'
+    bot.send_message(chat_id, result.rstrip('\n'))
+except BaseException as f:
+    bot.send_message(chat_id, f'\u2757\u2757\u2757 Scrypt error: {f}')
